7445aef4c6cfce6002fff929e4dcce4a7dc49a6d
Aymane Berhili <aberhili0man@gmail.com>
2026-03-30 13:55:00 +0100

    hmaach

--- file-researcher.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-grep "It takes 12 honey" file-researcher/facts.txt
-grep -n "year" file-researcher/facts.txt
--- head-and-tail.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl https://learn.zone01oujda.ma/assets/devops-branch/HeadTail.txt | head -1  && tail -1
