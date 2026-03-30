5d0ed3a31ed40e76faef14147512e5e04e599267
Aymane Berhili <aberhili0man@gmail.com>
2026-03-30 12:49:13 +0100

    test

--- json-researcher.sh
+++ json-researcher.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -s "https://learn.zone01oujda.ma/assets/superhero/all.json" | jq '.[] | select(.id == 1)' | grep -E '"name"|"power"'
+curl https://learn.zone01oujda.ma/assets/superhero/all.json | jq ".[] | select(.id == 1)" | grep -E "name|\"power\""
