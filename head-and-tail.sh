edbefdf5ca278fc91ae5b56ffc91c2f477e87a25
Aymane Berhili <aberhili0man@gmail.com>
2026-03-30 15:28:53 +0100

    same issue

--- head-and-tail.sh
+++ head-and-tail.sh
@@ -1,2 +1,2 @@
 
-curl https://learn.zone01oujda.ma/assets/devops-branch/HeadTail.txt >> tail.txt && head -1 tail.txt && tail -1 tail.txt
+curl -s https://learn.zone01oujda.ma/assets/devops-branch/HeadTail.txt >> tail.txt && head -1 tail.txt && tail -1 tail.txt
