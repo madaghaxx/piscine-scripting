cfccd9bdd50958df8eeceb205d4d3f0bb696cda7
Aymane Berhili <aberhili0man@gmail.com>
2026-04-05 11:22:59 +0100

    3gzt nktb commit

--- division.sh
+++ division.sh
@@ -2,17 +2,17 @@
 
 if [[ $# -ne 2 ]]; then
     echo "Error: two numbers must be provided"
-    exit 1
+    exit 0
 fi
 
 if ! [[ $1 =~ ^-?[0-9]+$ ]] || ! [[ $2 =~ ^-?[0-9]+$ ]]; then
     echo "Error: both arguments must be integers"
-    exit 1
+    exit 0
 fi
 
 if [[ $2 -eq 0 ]]; then
     echo "Error: division by zero is not allowed"
-    exit 1
+    exit 0
 fi
 
 echo "$(($1/$2))"
