b956955d89f140ebe1d2d60cb7638a6edb53ec66
Aymane Berhili <aberhili0man@gmail.com>
2026-04-05 11:38:48 +0100

    3gzt nktb commit

--- joker-num.sh
+++ joker-num.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
-count=5
-while [[ $count -gt 0 ]]; do
-    echo "Enter your guess ($count tries left)"
-    read guess
-    if [[ $guess -gt $1 ]]; then
-        echo "Go down"
-    elif [[ $guess -lt $1 ]]; then
-        echo "Go up"
-    else
-        echo "Congratulations, you found the number in $((6-count)) moves!"
-        break
+
+if [[ $# -ne 1 || ! "$1" =~ ^[0-9]+$ || "$1" -lt 1 || "$1" -gt 100 ]]; then
+    echo "Error: wrong argument"
+    exit 1
+fi
+
+secret="$1"
+
+for ((move=1; move<=5; )); do
+    tries_left=$((6 - move))
+    echo "Enter your guess ($tries_left tries left)"
+    read -r guess
+    if [[ ! "$guess" =~ ^[0-9]+$ || "$guess" -lt 1 || "$guess" -gt 100 ]]; then
+        continue
     fi
-    count=$(($count-1))
+
+    if (( guess > secret )); then
+        echo "Go down"
+        ((move++))
+    elif (( guess < secret )); then
+        echo "Go up"
+        ((move++))
+    else
+        echo "Congratulations, you found the number in $move moves!"
+        exit 0
+    fi
 done
+
+echo "You lost, the number was $secret"
