6e138b6516e73efa97f34a9eeed4a5d5aa1ce003
Aymane Berhili <aberhili0man@gmail.com>
2026-04-05 11:42:46 +0100

    3gzt nktb commit

--- calculator.sh
+++ calculator.sh
@@ -1,27 +1,38 @@
 #!/usr/bin/env bash
 
 error() {
-    >&2 echo "$1"
-    exit "$2"
+    printf '%s\n' "$1" >&2
+    exit 1
 }
 
-[ "$#" -eq 3 ] || error "Error: expect 3 arguments" 1
+[ "$#" -eq 1 ] || error "Error: expect 1 argument only!"
 
-num1=$1
-operator=$2
-num2=$3
+students=$1
+names=()
+grades=()
 
-[[ $num1 =~ ^-?[0-9]+$ && $num2 =~ ^-?[0-9]+$ ]] || error "Error: invalid number" 4
+for ((i = 1; i <= students; i++)); do
+    read -r -p "Student Name #$i: " name
+    read -r -p "Student Grade #$i: " grade
 
-case "$operator" in
-    "+") echo $((num1 + num2)) ;;
-    "-") echo $((num1 - num2)) ;;
-    "*") echo $((num1 * num2)) ;;
-    "/")
-        [ "$num2" -ne 0 ] || error "Error: division by 0" 2
-        echo $((num1 / num2))
-        ;;
-    *)
-        error "Error: invalid operator" 3
-        ;;
-esac
+    [[ $grade =~ ^[0-9]+$ ]] && [ "$grade" -le 100 ] \
+        || error "Error: The grade '$grade' is not a valid input. Only numerical grades between 0 and 100 are accepted."
+
+    names+=("$name")
+    grades+=("$grade")
+done
+
+for ((i = 0; i < students; i++)); do
+    name=${names[i]}
+    grade=${grades[i]}
+
+    if [ "$grade" -ge 90 ]; then
+        echo "$name: You did an excellent job!"
+    elif [ "$grade" -ge 70 ]; then
+        echo "$name: You did a good job!"
+    elif [ "$grade" -ge 50 ]; then
+        echo "$name: You need a bit more effort!"
+    else
+        echo "$name: You had a poor performance!"
+    fi
+done
