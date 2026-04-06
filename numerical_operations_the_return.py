cf32ab0a9d3155834ee4ad9eb926c0052c293104
Aymane Berhili <aberhili0man@gmail.com>
2026-04-06 15:54:07 +0100

    3gzt nktb commit

---
+++ numerical_operations_the_return.py
@@ -0,0 +1,13 @@
+
+def modulo(a, b):
+    return a%b
+
+def divide(a, b):
+    if b!= 0:
+        return a/b
+    return 0
+
+def integer_division(a, b):
+    if b!= 0:
+        return round(a/b)
+    return 0
